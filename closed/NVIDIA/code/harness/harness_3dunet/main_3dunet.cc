f9ecedacda568bac9ce2ee618f52a169056cfb6b
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-01-29 23:04:56 +0000

    Update main_3dunet.cc

--- closed/NVIDIA/code/harness/harness_3dunet/main_3dunet.cc
+++ closed/NVIDIA/code/harness/harness_3dunet/main_3dunet.cc
@@ -180,7 +180,8 @@ void doInference()
 
     gLogInfo << "mlperf.conf path: " << FLAGS_mlperf_conf_path << std::endl;
     gLogInfo << "user.conf path: " << FLAGS_user_conf_path << std::endl;
-    test_settings.FromConfig(FLAGS_mlperf_conf_path, FLAGS_model, FLAGS_scenario);
+    //not needed from v5.0
+    //test_settings.FromConfig(FLAGS_mlperf_conf_path, FLAGS_model, FLAGS_scenario);
     test_settings.FromConfig(FLAGS_user_conf_path, FLAGS_model, FLAGS_scenario);
 
     // Configure logging settings
